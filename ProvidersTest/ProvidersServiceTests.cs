f8b5928ace9c3facc20517f68151904c3a5e2e38
sabbond <sab@sabisme.uk>
2025-02-20 15:42:29 +0000

    Tests around ProviderService

--- ProvidersTest/ProvidersServiceTests.cs
+++ ProvidersTest/ProvidersServiceTests.cs
@@ -1,12 +1,48 @@
 namespace ProvidersTest;
+using Providers.Services;
+using Providers.Models;
+using Moq;
+using FluentAssertions;
 
 [TestClass]
 public class ProvidersServiceTests
 {
-    private IProviderCache cacheMock = new Mock<IProviderCache>();
+    private Mock<IProviderCache> cacheMock = new Mock<IProviderCache>();
+    private Mock<IProviderFeed> feedMock = new Mock<IProviderFeed>();
+    static string providerId1 = "id_00001";
+    private Provider provider1 = new Provider()
+    {
+        ProviderId = providerId1,
+        Name = "test provider 1",
+        LocationIds = []
+    };
 
     [TestMethod]
-    public void TestMethod1()
+    public async Task ProviderService_ReturnsCachedProvider()
     {
+        cacheMock.Setup(cache => cache.GetProvider(providerId1)).Returns(provider1);
+        var service = new ProviderService(feedMock.Object, cacheMock.Object);
+        var response = await service.GetProvider(providerId1);
+        response.Should().Be(provider1);
+    }
+
+    [TestMethod]
+    public async Task ProviderService_ReturnsFromFeedIfNothingInCache()
+    {
+        cacheMock.Setup(cache => cache.GetProvider(providerId1)).Returns<Provider>(null);
+        feedMock.Setup(feed => feed.GetProvider(providerId1).Result).Returns(provider1);
+        var service = new ProviderService(feedMock.Object, cacheMock.Object);
+        var response = await service.GetProvider(providerId1);
+        response.Should().Be(provider1);
+    }
+
+    [TestMethod]
+    public async Task ProviderService_StoresFeedResultInCache()
+    {
+        cacheMock.Setup(cache => cache.GetProvider(providerId1)).Returns<Provider>(null);
+        feedMock.Setup(feed => feed.GetProvider(providerId1).Result).Returns(provider1);
+        var service = new ProviderService(feedMock.Object, cacheMock.Object);
+        var response = await service.GetProvider(providerId1);
+        cacheMock.Verify(mock => mock.PutProvider(provider1), Times.Once());
     }
 }
